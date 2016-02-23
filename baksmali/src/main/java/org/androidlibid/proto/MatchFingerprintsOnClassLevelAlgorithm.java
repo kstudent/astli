a12b377f0e293027b25e7f4ad5a022941c4ae0cc
Christof Rabensteiner <christof.rabensteiner@student.tugraz.at>
2016-02-23 16:16:18 +0100

    Refactor: Moved Matching Task into Algorithm Class

--- baksmali/src/main/java/org/androidlibid/proto/MatchFingerprintsOnClassLevelAlgorithm.java
+++ baksmali/src/main/java/org/androidlibid/proto/MatchFingerprintsOnClassLevelAlgorithm.java
@@ -1,15 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.androidlibid.proto;
 
 import com.google.common.collect.Lists;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -18,6 +15,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.androidlibid.proto.ao.EntityService;
 import org.androidlibid.proto.ao.EntityServiceFactory;
+import org.androidlibid.proto.ao.VectorEntity;
+import org.androidlibid.proto.ast.ASTClassDefinition;
+import org.androidlibid.proto.ast.ASTToFingerprintTransformer;
+import org.androidlibid.proto.ast.Node;
 import org.jf.baksmali.baksmali;
 import org.jf.baksmali.baksmaliOptions;
 import org.jf.dexlib2.iface.ClassDef;
@@ -37,7 +38,7 @@ public class MatchFingerprintsOnClassLevelAlgorithm implements AndroidLibIDAlgor
     }
     @Override
     public boolean run() {
-        //        ExecutorService executor = Executors.newFixedThreadPool(options.jobs);
+//        ExecutorService executor = Executors.newFixedThreadPool(options.jobs);
         ExecutorService executor = Executors.newFixedThreadPool(1);
         List<Future<FingerPrintMatchTaskResult>> tasks = Lists.newArrayList();
 
@@ -83,6 +84,76 @@ public class MatchFingerprintsOnClassLevelAlgorithm implements AndroidLibIDAlgor
         }
         
         return true;
+    }
+    
+    class MatchClassFingerprintTask implements Callable<FingerPrintMatchTaskResult>{
+    
+        private final ClassDef classDef;
+        private final baksmaliOptions options;
+        private final EntityService service;
+
+        public MatchClassFingerprintTask(ClassDef classDef, baksmaliOptions options, EntityService service) {
+            this.classDef = classDef;
+            this.options = options;
+            this.service = service;
+        }
+
+        @Override public FingerPrintMatchTaskResult call() throws Exception {
+            String name = classDef.getType();
+
+            ASTClassDefinition classDefinition = new ASTClassDefinition(options, classDef);
+            List<Node> ast = classDefinition.createAST();
+
+            ASTToFingerprintTransformer ast2fpt = new ASTToFingerprintTransformer();
+
+            Fingerprint needle = new Fingerprint(name);
+
+            for(Node node : ast) {
+                Fingerprint methodFingerprint = ast2fpt.createFingerprint(node);
+                needle.add(methodFingerprint);
+            }
+
+            if (needle.euclideanNorm() == 0.0d) {
+                System.out.println(name + ": class length 0");
+                return FingerPrintMatchTaskResult.CLASS_LENGTH_0;
+            }
+
+            FingerprintMatcher matcher = new FingerprintMatcher(100.0d);
+
+            List<VectorEntity> classes = new ArrayList<VectorEntity>(service.getClasses());
+
+            FingerprintMatcher.Result result = matcher.matchFingerprints(classes, needle);
+
+            Fingerprint nameMatch = result.getMatchByName();
+            List<Fingerprint> matchesByDistance = result.getMatchesByDistance();
+
+            if(nameMatch == null) {
+                System.out.println(name + ": not mached by name");
+                return FingerPrintMatchTaskResult.NO_MATCH_BY_NAME;
+            } else {
+
+                int i;
+
+                for (i = 0; i < matchesByDistance.size(); i++) {
+                    if(matchesByDistance.get(i).getName().equals(name)) {
+                        break;
+                    }
+                }
+
+                if(i == matchesByDistance.size()) {
+                    System.out.println(name + ": not mached by distance.");
+                    System.out.println(needle);
+                    System.out.println(nameMatch);
+                    return FingerPrintMatchTaskResult.NO_MATCH_BY_DISTANCE;
+                } else if(i > 0) {
+                    System.out.println(name + ": found at position " + i);
+                    return FingerPrintMatchTaskResult.NOT_PERFECT;
+                } else {
+    //                System.out.println(name + ": found first.");
+                    return FingerPrintMatchTaskResult.OK;
+                }
+            }
+        }
     }
     
 }
