32ca8f7a0aeb808ddadc5c60867de614d24e4df4
Christof Rabensteiner <christof.rabensteiner@student.tugraz.at>
2016-04-07 19:32:03 +0200

    Test Matches on Class, Method and Package Level

--- baksmali/src/test/java/org/androidlibid/proto/match/MatchWithVectorDifferenceStrategyTest.java
+++ baksmali/src/test/java/org/androidlibid/proto/match/MatchWithVectorDifferenceStrategyTest.java
@@ -2,7 +2,6 @@ package org.androidlibid.proto.match;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,7 +12,15 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.androidlibid.proto.match.FingerprintMatcher.Result;
+import org.androidlibid.proto.match.MatchingStrategy.Status;
+import org.apache.commons.lang.StringUtils;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
 
 /**
  *
@@ -23,91 +30,137 @@ public class MatchWithVectorDifferenceStrategyTest {
 
     private FingerprintService service;
     private FingerprintMatcher matcher;
-    private Map<String, Fingerprint> needles;
-    private Fingerprint libPckg1, libPckg2, libPckg3, libPckg4;
+    private ResultEvaluator    evaluator;
     
+    private Map<String, Fingerprint> pckgNeedles, clssNeedles, mthdNeedles;
+    private List<Fingerprint> pckgsD2, pckgsD3, clsssD2, clsssD3, mthdsD2, mthdsD3;
+
     @Before
     public void setUp() throws SQLException {
         service   = mock(FingerprintService.class);
         matcher   = mock(FingerprintMatcher.class);
+        evaluator = mock(ResultEvaluator.class);
 
-        List<Fingerprint> packagesWithDepth2 = new ArrayList<>();
-        List<Fingerprint> packagesWithDepth3 = new ArrayList<>();
+        pckgsD2 = new ArrayList<>();
+        pckgsD3 = new ArrayList<>();
+        clsssD2 = new ArrayList<>();
+        clsssD3 = new ArrayList<>();
+        mthdsD2 = new ArrayList<>();
+        mthdsD3 = new ArrayList<>();
         
-        libPckg1 = addPrintToList(packagesWithDepth2, "tld.lib.pckg1",          1, 2,  3); 
-        libPckg2 = addPrintToList(packagesWithDepth2, "tld.lib.pckg2",          4, 5,  6); 
-        libPckg3 = addPrintToList(packagesWithDepth3, "tld.lib.pckg2.subPckgA", 7, 8,  9);
-        libPckg4 = addPrintToList(packagesWithDepth3, "tld.lib.pckg2.subPckgB", 7, 8, 10);
+        pckgNeedles = new HashMap<>();
+        clssNeedles = new HashMap<>();
+        mthdNeedles = new HashMap<>();
         
-        when(service.findPackagesByDepth(2)).thenReturn(packagesWithDepth2);
-        when(service.findPackagesByDepth(3)).thenReturn(packagesWithDepth3);
+        for(String needleName : new String[]{"a.b.c", "a.b.c.d", "a.b.d.a"}) {
+            createAndAddFingerprintsToNeedleMaps(needleName); 
+        }
+
+        for(String libName : new String[]{
+            "tld.lib.pckg1", 
+            "tld.lib.pckg2", 
+            "tld.lib.pckg2.subPckgA", 
+            "tld.lib.pckg2.subPckgB"}) 
+        {
+            createAndAddFingerprintsToDepthLists(libName); 
+        }
         
-        needles = new HashMap<>();
-        Fingerprint needle0 = addPrintToMap(needles, "a.b.c",   1, 2, 3); 
-        Fingerprint needle1 = addPrintToMap(needles, "a.b.d",   4, 5, 6); 
-        Fingerprint needle2 = addPrintToMap(needles, "a.b.d.a", 7, 8, 9);
+        when(service.findPackagesByDepth(2)).thenReturn(pckgsD2);
+        when(service.findPackagesByDepth(3)).thenReturn(pckgsD3);
+        when(service.findClassesByPackageDepth(2)).thenReturn(clsssD2);
+        when(service.findClassesByPackageDepth(3)).thenReturn(clsssD3);
+        when(service.findMethodsByPackageDepth(2)).thenReturn(mthdsD2);
+        when(service.findMethodsByPackageDepth(3)).thenReturn(mthdsD3);
         
-        when(matcher.matchFingerprints(packagesWithDepth2, needle0))
-                .thenReturn(createResult(needle0, null, libPckg1, libPckg2));
+        when(matcher.matchFingerprints(any(List.class), any(Fingerprint.class))).thenAnswer(new Answer<Result>(){
+            @Override
+            public Result answer(InvocationOnMock invocation) throws Throwable {
+                List<Fingerprint> hayStack = (List<Fingerprint>) invocation.getArguments()[0];
+                Fingerprint needle = (Fingerprint) invocation.getArguments()[1];
+                Result result = new Result();
+                result.setNeedle(needle);
+                result.setMatchesByDistance(hayStack);
+                return result;
+            }
+        });
         
-        when(matcher.matchFingerprints(packagesWithDepth2, needle1))
-                .thenReturn(createResult(needle1, null, libPckg2, libPckg1));
-        
-        when(matcher.matchFingerprints(packagesWithDepth3, needle2))
-                .thenReturn(createResult(needle2, null, libPckg3, libPckg4));
-        
+        when(evaluator.evaluateResult(any(Fingerprint.class), any(Result.class))).thenReturn(Status.OK);
+
     }
     
     @Test
     public void testMatchOnPackageLevel() throws SQLException {
         Level level = Level.PACKAGE;
-        
-        ResultEvaluator evaluator = new ResultEvaluator() {
-            @Override
-            public MatchingStrategy.Status evaluateResult(Fingerprint needle, Result result) {
-                
-                if(needle.equals(needles.get("a.b.c"))) {
-                    assert(result.getMatchesByDistance().get(0).equals(libPckg1));
-                } else if(needle.equals(needles.get("a.b.d"))) {
-                    assert(result.getMatchesByDistance().get(0).equals(libPckg2));
-                } else if(needle.equals(needles.get("a.b.d.a"))) {
-                    assert(result.getMatchesByDistance().get(0).equals(libPckg3));
-                } else {
-                    assert(false);
-                }
-                return MatchingStrategy.Status.OK;
-            }
-        };
-        
+                 
         MatchingStrategy strategy = new MatchWithVectorDifferenceStrategy(
             service, evaluator, matcher, level);
                 
-        Map<MatchingStrategy.Status, Integer> stats = strategy.matchPrints(needles);
+        Map<MatchingStrategy.Status, Integer> stats = strategy.matchPrints(pckgNeedles);
+        
+        verify(matcher, times(3)).matchFingerprints(any(List.class), any(Fingerprint.class));
         
         assert(stats.get(MatchingStrategy.Status.OK).equals(3));
-        
     }
     
-    private Fingerprint addPrintToList(List<Fingerprint> list, String name, int... values) {
-        Fingerprint print = new Fingerprint(name);
-        print.setFeatureValues(values);
-        list.add(print);
-        return print;
-    }
-
-    private Fingerprint addPrintToMap(Map<String, Fingerprint> map, String name, int... values) {
-        Fingerprint print = new Fingerprint(name);
-        print.setFeatureValues(values);
-        map.put(name, print);
-        return print;
-    }
-
-    private Result createResult(Fingerprint needle, Fingerprint matchByName, Fingerprint... matchesByDistance) {
-        Result result = new Result();
-        result.setNeedle(needle);
-        result.setMatchByName(matchByName);
-        result.setMatchesByDistance(Arrays.asList(matchesByDistance));
+    @Test
+    public void testMatchOnClassLevel() throws SQLException {
+        Level level = Level.CLASS;
+                 
+        MatchingStrategy strategy = new MatchWithVectorDifferenceStrategy(
+            service, evaluator, matcher, level);
+                
+        Map<MatchingStrategy.Status, Integer> stats = strategy.matchPrints(pckgNeedles);
         
-        return result;
+        verify(matcher, times(3)).matchFingerprints(any(List.class), any(Fingerprint.class));
+        
+        assert(stats.get(MatchingStrategy.Status.OK).equals(3));
+    }
+    
+    @Test
+    public void testMatchOnMethodLevel() throws SQLException {
+        Level level = Level.METHOD;
+                 
+        MatchingStrategy strategy = new MatchWithVectorDifferenceStrategy(
+            service, evaluator, matcher, level);
+                
+        Map<MatchingStrategy.Status, Integer> stats = strategy.matchPrints(pckgNeedles);
+        
+        verify(matcher, times(3)).matchFingerprints(any(List.class), any(Fingerprint.class));
+        
+        assert(stats.get(MatchingStrategy.Status.OK).equals(3));
+    }
+    
+    private void createAndAddFingerprintsToDepthLists(String name) {
+        int depth = StringUtils.countMatches(name, ".");
+        
+        Fingerprint pckgPrint = new Fingerprint(name);
+        Fingerprint clssPrint = new Fingerprint(name + ":Class");
+        Fingerprint mthdPrint = new Fingerprint(name + ":Class:method()");
+
+        pckgPrint.addChildFingerprint(clssPrint);
+        clssPrint.addChildFingerprint(mthdPrint);
+        
+        if(depth == 2) {
+            pckgsD2.add(pckgPrint);
+            clsssD2.add(clssPrint);
+            mthdsD2.add(mthdPrint);
+        } else {
+            pckgsD3.add(pckgPrint);
+            clsssD3.add(clssPrint);
+            mthdsD3.add(mthdPrint);
+        }
+    }
+
+    private void createAndAddFingerprintsToNeedleMaps(String name) {
+        Fingerprint pckgPrint = new Fingerprint(name);
+        Fingerprint clssPrint = new Fingerprint(name + ":Class");
+        Fingerprint mthdPrint = new Fingerprint(name + ":Class:method()");
+        
+        pckgPrint.addChildFingerprint(clssPrint);
+        clssPrint.addChildFingerprint(mthdPrint);
+        
+        pckgNeedles.put(pckgPrint.getName(), pckgPrint);
+        clssNeedles.put(clssPrint.getName(), clssPrint);
+        mthdNeedles.put(mthdPrint.getName(), mthdPrint);
     }
 }
